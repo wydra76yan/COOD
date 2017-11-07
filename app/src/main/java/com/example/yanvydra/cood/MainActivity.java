7681997130c08e5e1bed75e983e5962ba2a25ab9
wydra76yan <yan@vydra.com>
2017-11-07 21:57:01 +0300

    Added pre-start and logo

--- app/src/main/java/com/example/yanvydra/cood/MainActivity.java
+++ app/src/main/java/com/example/yanvydra/cood/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.yanvydra.cood;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 7000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView) findViewById(R.id.logo);
+        Animation appearance = AnimationUtils.loadAnimation(this,R.anim.logo_appearance);
+        imageView.setAnimation(appearance);
+
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+            }
+        },SPLASH_TIME_OUT);
     }
 }
+
+
